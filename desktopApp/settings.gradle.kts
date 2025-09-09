38f9402355abd5d139739d86a61552134b9ee587
Albirich <Shirigama@gmail.com>
2025-09-09 19:55:04 -0400

    Update settings.gradle.kts

--- desktopApp/settings.gradle.kts
+++ desktopApp/settings.gradle.kts
@@ -4,7 +4,15 @@ pluginManagement {
         mavenCentral()
         google()
     }
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "2.0.21"
+        id("org.jetbrains.kotlin.android") version "2.0.21"   // if you use it in :app
+        id("org.jetbrains.compose") version "1.7.0"
+        id("com.android.application") version "8.9.1"          // whatever you already use
+        id("com.google.gms.google-services") version "4.4.2"   // if present in root/app
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
